837a86920f7ce497588dd8a750cb7b9b19d7f115
bunnie <bunnie@kosagi.com>
2020-06-07 16:01:30 +0100

    add a "jig is on" LED

--- run_exclave.sh
+++ run_exclave.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+echo "26" > /sys/class/gpio/export
+sleep 1
+echo out > /sys/class/gpio/gpio26/direction
+sleep 1
+echo 1 > /sys/class/gpio/gpio26/value
+sleep 1
+
 /home/pi/.cargo/bin/exclave -c /home/pi/code/jig20-config-bm17
